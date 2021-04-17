b3ee3dcb41c748791bc2fd7b3b2f23ec16f6eee7
Nam Huynh <nam@capogrecogroup.com>
2021-04-17 16:49:48 +0700

    Remove /public path

--- nam-design.js
+++ nam-design.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.use("/public", express.static("public"));
+app.use("/", express.static("public"));
 
 app.listen(10003, () => {
-  console.log("App is running on port 3000");
+  console.log("App is running on port 10003");
 });
