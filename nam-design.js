a4301881e46a27d8c57482440a281632377f1c56
Nam Huynh <nam@capogrecogroup.com>
2021-09-30 11:02:04 +0700

    Redirect /nguyentrung to homepage

--- nam-design.js
+++ nam-design.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use("/nguyentrung/", (req, res) => {
+  res.redirect("/");
+});
+
 app.use("/", express.static("public"));
 
 app.listen(10003, () => {
